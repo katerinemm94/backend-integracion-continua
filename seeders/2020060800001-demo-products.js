804cfb5e094d8155d55864af15a3fbf798e1a56c
Katerine Martinez <katerinemm94@gmail.com>
2020-07-07 19:53:30 -0500

    Cambios en los seeders

--- seeders/2020060800001-demo-products.js
+++ seeders/2020060800001-demo-products.js
@@ -1,23 +1,38 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('products', [{
-      id: 1,
-      code: 'H76TYT0',
-      description: 'Pipeta exosto mofle universal akrapovic',
-      unitPrice: 190000,
-      quantityPerBox: 3,
-      boxPrice: 480000,
-      packagingType: false,
-      numberModels: 3,
-      notes: 'N/A',
-      primaryPhotoURL: null,
-      thumbnailPhotoUrl: null,
-      status: true,
-      createdAt: '2020-06-07 08:32:24',
-      updatedAt: '2020-06-07 08:32:24'
-    }], {});
+
+  up: async (queryInterface, Sequelize) => {
+
+    const product = await queryInterface.rawSelect('products', {
+      where: {
+        id: 1,
+      },
+    }, ['id']);
+
+    if (!product) {
+      
+      return queryInterface.bulkInsert('products', [{
+        id: 1,
+        code: 'H76TYT0',
+        description: 'Pipeta exosto mofle universal akrapovic',
+        unitPrice: 190000,
+        quantityPerBox: 3,
+        boxPrice: 480000,
+        packagingType: false,
+        numberModels: 3,
+        notes: 'N/A',
+        primaryPhotoURL: null,
+        thumbnailPhotoUrl: null,
+        status: true,
+        createdAt: '2020-06-07 08:32:24',
+        updatedAt: '2020-06-07 08:32:24'
+      }], {});
+
+    } else {
+      return;
+    }
+
   },
 
   down: (queryInterface, Sequelize) => {
--- seeders/2020060800002-demo-providers.js
+++ seeders/2020060800002-demo-providers.js
@@ -1,21 +1,36 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('providers', [{
-      id: 1,
-      code: 'WE879J',
-      name: 'MotoWork la 33',
-      city: 'Medellìn',
-      country: 'Colombia',
-      whatsapp: '3147889865',
-      telephone: '5033328',
-      primaryPhotoURL: null,
-      notes: 'N/A',
-      status: true,
-      createdAt: '2020-06-07 08:32:24',
-      updatedAt: '2020-06-07 08:32:24'
-    }], {});
+
+  up: async (queryInterface, Sequelize) => {
+
+    const provider = await queryInterface.rawSelect('providers', {
+      where: {
+        id: 1,
+      },
+    }, ['id']);
+
+    if (!provider) {
+      
+      return queryInterface.bulkInsert('providers', [{
+        id: 1,
+        code: 'WE879J',
+        name: 'MotoWork la 33',
+        city: 'Medellìn',
+        country: 'Colombia',
+        whatsapp: '3147889865',
+        telephone: '5033328',
+        primaryPhotoURL: null,
+        notes: 'N/A',
+        status: true,
+        createdAt: '2020-06-07 08:32:24',
+        updatedAt: '2020-06-07 08:32:24'
+      }], {});
+
+    } else {
+      return;
+    }
+
   },
 
   down: (queryInterface, Sequelize) => {
